461cb93197903d848e32dd406ed5e8254a5f0f38
cohennamit <123540145+cohennamit@users.noreply.github.com>
2023-01-25 17:10:12 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,26 +5,41 @@ const MINE = '💣'
 var gBoard
 
 var gLevel = {
-    Size: 4,
-    Mines: 2,
+    size: 8,
+    mines: 14,
 }
 
 var gGame = {
     isOn: false,
+    turnCount: 0,
     shownCount: 0,
     markedCount: 0,
     secsPassed: 0,
+    livesCount: 3,
 }
 
+var elLivesSpan = document.querySelector('.lives')
+var elSmileyBtn = document.querySelector('.smiley-btn')
+
 function onInit() {
-    console.log('Hello!')
+    // Before first 
+    elSmileyBtn.innerText = '😃'
     gGame.isOn = true
-    gBoard = buildBoard(gLevel.Size)
+    gGame.turnCount = 0
+    gGame.shownCount = 0
+    gGame.markedCount = 0
+    gGame.secsPassed = 0
+    gGame.livesCount = 3
+    elLivesSpan.innerText = gGame.livesCount
+    gBoard = buildBoard(gLevel.size)
+    renderBoard(gBoard, '.board-container')
+    // After first turn
     addMines(gBoard)
     setMinesNegsCount(gBoard)
     renderBoard(gBoard, '.board-container')
-
+    console.log('gBoard', gBoard)
 }
+
 
 function buildBoard(num) {
     var minesAmount = 2
@@ -54,8 +69,18 @@ function setMinesNegsCount(board) {
 }
 
 function onCellClicked(elCell, i, j) {
+
+    if (gBoard[i][j].isShown || !gGame.isOn) return
+
+    gGame.turnCount++
+    console.log('gGame.turnCount', gGame.turnCount)
     if (gBoard[i][j].isMine) {
-        revealBombs()
+        revealCell(elCell, i, j)
+        gGame.livesCount--
+        elLivesSpan.innerText = gGame.livesCount
+        if (gGame.livesCount === 0) {
+            gameOver()
+        }
     } else if (gBoard[i][j].minesAroundCount === 0) {
         revealCell(elCell, i, j)
         revealNegs(i, j)
@@ -64,20 +89,11 @@ function onCellClicked(elCell, i, j) {
     }
 }
 
-function revealBombs() {
-    for (var i = 0; i < gBoard.length; i++) {
-        for (var j = 0; j < gBoard[i].length; j++) {
-            var currCell = gBoard[i][j]
-            if (currCell.isMine) {
-                currCell.isShown = true
-                var elCell = document.querySelector(`.cell-${i}-${j}`)
-                revealCell(elCell, i, j)
-
-            }
-        }
-    }
+function gameOver() {
+    gGame.isOn = !gGame.isOn
+    elSmileyBtn.innerText = '🤯'
+    revealBombs()
 }
-
 
 function revealNegs(rowIdx, colIdx) {
 
@@ -94,7 +110,6 @@ function revealNegs(rowIdx, colIdx) {
     }
 }
 
-
 function revealCell(elCell, i, j) {
     gBoard[i][j].isShown = true
     var elCellSpan = elCell.querySelector('.hidden')
@@ -102,19 +117,18 @@ function revealCell(elCell, i, j) {
         elCellSpan.classList.remove('hidden')
         elCell.classList.add('clicked')
         gGame.shownCount++
-        console.log('gGame.shownCount', gGame.shownCount)
     }
     else return
 }
 
 function addMines(board) {
-    board[1][1].isMine = true
-    board[3][3].isMine = true
-    // for (var i = 0; i < gLevel.Mines; i++) {
-    //     const emptyPos = getEmptyPos()
-    //     if (!emptyPos) return
-    //     gBoard[emptyPos.i][emptyPos.j].isMine = true
-    // }
+    // board[1][1].isMine = true
+    // board[3][3].isMine = true
+    for (var i = 0; i < gLevel.mines; i++) {
+        const emptyPos = getEmptyPos()
+        if (!emptyPos) return
+        gBoard[emptyPos.i][emptyPos.j].isMine = true
+    }
 
 }
 
@@ -122,11 +136,25 @@ function getEmptyPos() {
     const emptyPoss = []
     for (var i = 0; i < gBoard.length; i++) {
         for (var j = 0; j < gBoard[0].length; j++) {
-            if (!gBoard[i][j].isMine) {
+            if (!gBoard[i][j].isMine && !gBoard[i][j].isShown) {
                 emptyPoss.push({ i, j })
             }
         }
     }
     var randIdx = getRandomInt(0, emptyPoss.length)
     return emptyPoss[randIdx]
+}
+
+function revealBombs() {
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[i].length; j++) {
+            var currCell = gBoard[i][j]
+            if (currCell.isMine) {
+                currCell.isShown = true
+                var elCell = document.querySelector(`.cell-${i}-${j}`)
+                revealCell(elCell, i, j)
+
+            }
+        }
+    }
 }
